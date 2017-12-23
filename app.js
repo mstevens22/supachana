32b13005fb41afae6ac6e0c2fa5170f9ba2a71c3
mathias stevens <mathiasstevens@air-de-mathias.home>
2017-12-23 22:12:52 +0100

    End with sms

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const express = require('express');
 const app = express();
 
 let google = require('googleapis');
+let config = require("./config.json");
 let privatekey = require("./supanacha-2-a45bf07275b1.json");
+
+//Todo: imagine a setup through localized json files (ga:id, cron frequency, locale, etc..) 
 
 // configure a JWT auth client
 let jwtClient = new google.auth.JWT(
@@ -37,33 +40,46 @@ jwtClient.authorize(function (err, tokens) {
    console.log(err);
    return;
  } else {
-   console.log("Successfully connected!");
+   console.log("Successfully connected! to google apis");
  }
 });
 
 var analytics = google.analytics('v3');
 
-app.get('/', (req, res) => {
-	console.log(req.headers);
-	if(req.headers['x-appengine-cron']){
+const clientTwilio = require('twilio')(
+  config.twilio_sid,
+  config.twilio_token
+);
+
+app.get('/', (req, res) => {	
+	//if(req.headers['x-appengine-cron']){//Only cronjob can access
 		analytics.data.realtime.get({
-        auth: jwtClient,
-        'ids': 'ga:115201053',
-        //'metrics': 'rt:goal1Completions',
-        'metrics': 'rt:pageviews',
-        'dimensions': 'rt:minutesAgo',
-        'prettyPrint': true
-    }, function(err, result) {
-        console.log(err);
-        if (result) {
-        	for(var i = 0; i < result.rows.length;i++){
-		        console.log(result.rows[i]);
-		  	}
-        }        
-    });
+	        auth: jwtClient,
+	        'ids': 'ga:115201053',
+	        //'metrics': 'rt:goal1Completions',
+	        'metrics': 'rt:pageviews',
+	        'dimensions': 'rt:minutesAgo',
+	        'prettyPrint': true
+    	}, function(err, result) {    		
+	        if (result) {
+	        	var success = false;
+	        	for (let item of result.rows) {	        		
+	        		if (item[0] < config.delay) {
+	        			console.log(item);
+	        		}				  
+				}
+	        } 
+			/*clientTwilio.messages.create({
+			  from: config.twilio_from,
+			  to: config.twilio_to,
+			  body: JSON.stringify(result)
+			}).then((messsage) => console.log(message.sid)); */     
+    	});
+
 		res.status(200).send('====SUPACHANA===$').end();
-	}
-  res.status(401).send('====SUPACHANA===$').end();
+	/*} else {
+		res.status(401).send('====SUPACHANA===$').end();
+	}*/
 });
 
 // Start the server
